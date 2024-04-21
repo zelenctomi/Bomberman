8d8d526d74e6e7c8d5420676d5f01b7585ccfc13
Jóna Kristóf <x3f10s@inf.elte.hu>
2024-04-21 20:47:13 +0200

    CI-update-v3

--- test/tests.py
+++ test/tests.py
@@ -1,4 +1,5 @@
-'''
+
+import pygame
 import unittest
 import sys
 import os
@@ -6,29 +7,19 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from fields import *
 
-# This is the function you want to test
-def add(a, b):
-    return a + b
+class TestStringMethods(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
-# This is your test case class
-class TestAddFunction(unittest.TestCase):
-    def test_add(self):
-        # Test if 1 + 1 equals 2
-        self.assertEqual(add(1, 1), 2)
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
 
-        # Test if -1 + 1 equals 0
-        self.assertEqual(add(-1, 1), 0)
-
-    #def spawnTest(self):
-    #    test_fields: Fields = Fields()
-    #    test_spawner: Spawner = Spawner(test_fields)
-    #    test_players: list[Player] = test_spawner.spawn_players([Settings.P1_CONTROLS, Settings.P2_CONTROLS, Settings.P3_CONTROLS])
-
-    def fields_test_get_crumbly_walls(self):
+    def test_fields_test_get_crumbly_walls(self):
         test_fields: Fields = Fields()
         self.assertEqual(test_fields.get_crumbly_walls(), [])
 
-# This allows the test to be run
 if __name__ == '__main__':
     unittest.main()
-'''
+
+
