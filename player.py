5f29ca08ef767a9d38ce66d738e8e96107db2860
Zelenc-Kuma Tamás <vijikh@inf.elte.hu>
2024-04-23 16:58:06 +0000

    Revert "speed_1.0"
    This reverts commit b91eedfb694aac8db1168f5ffcce300cb89c36ac

--- player.py
+++ player.py
@@ -18,8 +18,7 @@ class Player:
       'bomb': 1,
       'explosion': 2,
       'detonator': 0,
-      'invulnerability': 0,
-      'speed': 0
+      'invulnerability': 0
     }
     # Animation #
     self.frame: int = 0
@@ -148,8 +147,6 @@ class Player:
         y += 1
 
       x, y = self.__move_or_collide(x, y)
-      if self.stats['speed'] > 0:
-        x, y = self.__move_or_collide(x, y)
 
       if x != 0 or y != 0:
         self.prevDirection = self.direction
@@ -244,9 +241,11 @@ class Player:
     value: int
     stat, value = powerup.get_bonus()
     self.stats[stat] += value
-    if stat == 'invulnerability':
-      self.__start_unvulnerability_timer
-    ##elif stat == 'speed'
+    self.__check_stats()
+    
+  def __check_stats(self):
+    if self.stats['invulnerability'] > 0:
+      self.__start_unvulnerability_timer()
 
   def __start_unvulnerability_timer(self):
     timer = threading.Timer(20, self.__countdown, [20])
--- powerups.py
+++ powerups.py
@@ -4,7 +4,6 @@ from extra_bomb import Extra_bomb
 from longer_explosion import Longer_explosion
 from detonator import Detonator
 from invulnerability import Invulnerability
-from speed import Speed
 
 
 class Powerups:
@@ -14,8 +13,7 @@ class Powerups:
     POWERUPS: list[Powerup] = [Extra_bomb(coord, size), 
                                Longer_explosion(coord, size), 
                                Detonator(coord, size),
-                               Invulnerability(coord, size),
-                               Speed(coord, size)]
+                               Invulnerability(coord, size)]
     PROBABILITY: int = 5
     r: int = random.randint(0, PROBABILITY * len(POWERUPS) - PROBABILITY)
     if r > len(POWERUPS) - 1:
